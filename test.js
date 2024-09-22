f9fc01647e72a000ce7dde65b2994072790d1853
Nirbhay Kumar <nirbhay.kr07@gmail.com>
2024-09-22 17:03:48 +0000

    second

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("Hello Worlds");
+const acc = "12";
+//var name="Nirbhay"
+let roll = 12
+let state
+console.log(acc);
+// alert("Hello")
+// acc = 112;
+console.table([acc, roll, state])
+console.log(typeof name)
